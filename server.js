0688780175db5ac098de836ce1471df1aceb82f6
sagar <sagar@bluelupin.com>
2022-05-09 13:23:37 +0530

    algorithm rounds updated

--- server.js
+++ server.js
@@ -8,8 +8,9 @@
 
 // ------------------------------------------------------------
 
-
-var NUMBER_OF_PARTICIPANTS = 7; // Set the number of participants
+const readline = require('readline')
+ var RoundWinners = []
+var NUMBER_OF_PARTICIPANTS = 14 ; // Set the number of participants
 if (!String.prototype.format) {
     String.prototype.format = function () {
         var args = arguments;
@@ -19,11 +20,11 @@ if (!String.prototype.format) {
     };
 }
 
-var participants = Array.from({ length: NUMBER_OF_PARTICIPANTS }, ( l,p) => p + 1);
+var participants = Array.from({ length: NUMBER_OF_PARTICIPANTS }, ( _l,p) => p + 1);
 var bracket = getBracket(participants);
+var roundMatches
 
 console.log(bracket);
-
 function getBracket(participants) {
     var participantsCount = participants.length;
     // math.ceil() is basically used for round off number 
@@ -41,11 +42,10 @@ function getBracket(participants) {
     if (participantsCount < 2) {
         return [];
     }
-
     var matches = [[1,2]];
 
     for (var round = 1; round < rounds; round++) {
-        var roundMatches = [];
+         roundMatches = [];
 
         // Math.pow(2, round + 1) + 1; means [ (2 to the power round +1 ), =>  lets round = 3 so 2 to the power 3=>8 +1 = 9 ] 
         var sum = Math.pow(2, round + 1) + 1;
@@ -66,9 +66,56 @@ function getBracket(participants) {
 }
 
 function changeIntoBye(seed, participantsCount) {
-    // return seed <= participantsCount ?  seed : '{0} (= bye)'.format(seed);  
+    //  return seed <= participantsCount ?  seed : '{0} (= bye)'.format(seed);  
      return seed <= participantsCount ? seed : null;
 }
+
+// -------------------------for first round winner ---------------------------------- 
+// let input = [ [ 1, 8 ], [ 5, 4 ], [ 3, 6 ], [ 7, 2 ] ]
+let input = [[]]
+input = roundMatches
+
+let winners=[]
+// let firstWinner
+    async function winnersFunction(question) {
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })  
+      
+      return new Promise((res)=>rl.question(question, ans => {
+        rl.close();
+        res(ans)
+      }))
+    }
+
+    (async function getWinners() {
+        for(let i=0; i<input.length;i++) {
+            if(!(input[i][0] && input[i][1])){
+                 console.log(`winner is ${input[i][0]||input[i][1]}`)
+                 winners.push(input[i][0]||input[i][1])
+            } else {
+                winners.push(await winnersFunction(`winner from ${input[i][0]} and ${input[i][1]} :`))
+                // console.log(winners)
+                
+            }
+        }
+        RoundWinners.push(winners)
+        console.log(`First Round Winners are :` ,RoundWinners);
+        return winners
+    })()
+// ---------------second round winner-------------
+//  input = RoundWinners
+//  let Winners =[]
+// let seconWinner
+//  async function WineerFunction(question){
+//  const rl = readline.createInterface({ input: process.stdin, output : process.stdout})
+//  return new Promise((res)=>rl.question(question,ans =>{
+//     rl.close();
+//     res(ans)
+//  }))}
+  
 
 
 
